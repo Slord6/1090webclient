542d01a04db77edaeb840193b9db4e961a39aef3
Sam Lord <samuel.lord@students.plymouth.ac.uk>
2019-05-18 13:05:08 +0100

    Refactored plane markers into its own function
    Better documentation

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-// Leaflet map setup
+// === Leaflet map setup ===
 var planeMap = L.map('planeMap').setView([50.27, -3.70], 8);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -7,15 +7,27 @@ var markers = L.layerGroup().addTo(planeMap);
 
 // Heatmap setup
 var heat = L.heatLayer([]).addTo(planeMap);
-// On/off toggle for heatmap
-var heatMapActive = true;
 
+// === End map setup ===
+
+// === Settings ===
 // Time between data fetches in seconds
 let dataFetchBreakTime = 1;
 // How long to wait until we remove planes from the map (seconds)
 let planeTimeout = 60;
 // In-mem plane storage
 let activePlanes = {};
+// On/off toggle for heatmap
+var heatMapActive = true;
+// === End Settings
+
+/**
+ * Remove plane from the system
+ * @param {*} plane 
+ */
+let removePlane = (plane) => {
+    delete activePlanes[plane.hex];
+}
 
 /**
  * Fetch new data from the server
@@ -72,15 +84,7 @@ let updateMap = () => {
                 console.log('New plane!', plane.hex, plane.flight, plane.validposition ? 'with position' : 'no position', plane.firstSeen);
             }
             if(plane.validposition) {       
-                let newMarker = new L.Marker([plane.lat, plane.lon], {
-                    icon: new L.DivIcon({
-                        className: 'infoPanel',
-                        html: '<img style="transform: rotate(' + plane.track + 'deg)" src="./media/plane_black.png"/>' +
-                              '<div class="infoText">' + planeToInfoPanel(plane) + '</div>'
-                    })
-                });
-                markers.addLayer(newMarker).addTo(planeMap);
-
+                addPlaneMarker(plane);
                 updateHeatmapLayer(plane);
             }
         });
@@ -96,6 +100,20 @@ let updateMap = () => {
         if(heatMapActive && plane.seen < dataFetchBreakTime + 1) {
             heat.addLatLng([plane.lat, plane.lon]);
         }
+    }
+    /**
+     * Add a plane icon and hover info panel
+     * @param {*} plane 
+     */
+    let addPlaneMarker = (plane) => {
+        let newMarker = new L.Marker([plane.lat, plane.lon], {
+        icon: new L.DivIcon({
+            className: 'infoPanel',
+            html: '<img style="transform: rotate(' + plane.track + 'deg)" src="./media/plane_black.png"/>' +
+                  '<div class="infoText">' + planeToInfoPanel(plane) + '</div>'
+        })
+    });
+    markers.addLayer(newMarker).addTo(planeMap);
     }
     /**
      * Used as callback for getNewData
@@ -140,7 +158,7 @@ let updateMap = () => {
         // Remove out of date planes
         Object.values(newPlanes).forEach((plane, index) => {
             if(plane.seen > planeTimeout){
-                delete activePlanes[plane.hex];
+                removePlane(plane);
             }
         });
         drawMarkers(activePlanes);
